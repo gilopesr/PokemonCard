efe72244f7088d77ba4a0fe5624f7f5b344af886
Giovana Lopes <gilopes2005@gmail.com>
2024-11-16 13:00:36 -0300

    Add files via upload

---
+++ pokemon/index.js
@@ -0,0 +1,21 @@
+async function Buscarpokemon() {
+    let idPokemon = document.getElementById('id-pokemon').value
+
+    const id = idPokemon ? idPokemon : alert('Digite Um Número')
+
+    let url = "https://pokeapi.co/api/v2/pokemon-form/"+id;
+    let api = await fetch(url,{
+        method: 'GET'
+    })
+
+    if (api.ok){
+        let resposta = await api.json();
+        document.getElementById('txt-name').innerHTML = resposta.name;
+        document.getElementById('txt-id').innerHTML = resposta.id;
+        document.getElementById('img-perfil').src = resposta.sprites.back_default;
+
+    }
+}
+
+Buscarpokemon()
+
