d4f10999f9721537c7959b1ad898abce9eff62e8
Giovana Lopes <gilopes2005@gmail.com>
2024-11-27 22:58:16 -0300

    Update index.js

--- pokemon/index.js
+++ pokemon/index.js
@@ -1,6 +1,8 @@
 async function Buscarpokemon() {
     let idPokemon = document.getElementById('id-pokemon').value
 
+
+    // condição ? valor_se_verdadeiro : valor_se_falso;
     const id = idPokemon ? idPokemon : alert('Digite Um Número')
 
     let url = "https://pokeapi.co/api/v2/pokemon-form/"+id;
@@ -10,6 +12,9 @@ async function Buscarpokemon() {
 
     if (api.ok){
         let resposta = await api.json();
+
+        console.log(resposta); //resposta da api no console
+
         document.getElementById('txt-name').innerHTML = resposta.name; //name é a resposta da api
         document.getElementById('txt-id').innerHTML = resposta.id;
         document.getElementById('img-perfil').src = resposta.sprites.back_default;
