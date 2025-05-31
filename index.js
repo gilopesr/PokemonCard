3a8f8ba561a500ce54185147589a4841cf2f6fc3
Giovana Lopes <gilopes2005@gmail.com>
2025-05-30 23:14:22 -0300

    nova versão

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 async function Buscarpokemon() {
     let idPokemon = document.getElementById('id-pokemon').value
 
-
-    // condição ? valor_se_verdadeiro : valor_se_falso;
     const id = idPokemon ? idPokemon : alert('Digite Um Número')
 
     let url = "https://pokeapi.co/api/v2/pokemon-form/"+id;
@@ -10,17 +8,46 @@ async function Buscarpokemon() {
         method: 'GET'
     })
 
-    if (api.ok){
+    if (api.ok) {
         let resposta = await api.json();
 
-        console.log(resposta); //resposta da api no console
+        console.log(resposta);
 
         document.getElementById('txt-name').innerHTML = resposta.name;
         document.getElementById('txt-id').innerHTML = resposta.id;
-        document.getElementById('img-perfil').src = resposta.sprites.back_default;
+        document.getElementById('img-perfil').src = resposta.sprites.front_default;
 
+        if (resposta.types && resposta.types.length > 0) {
+            document.getElementById('txt-tipo').innerHTML = resposta.types[0].type.name;
+        } else {
+            document.getElementById('txt-tipo').innerHTML = 'N/A';
+        }
+
+        const speciesUrl = 'https://pokeapi.co/api/v2/pokemon-species/'+id;
+        let speciesApi = await fetch(speciesUrl, {
+            method: 'GET'
+        });
+
+        if (speciesApi.ok) {
+            let speciesResposta = await speciesApi.json();
+
+
+            if (speciesResposta.color && speciesResposta.color.name) {
+                document.getElementById('txt-cor').innerHTML = speciesResposta.color.name;
+            } else {
+                document.getElementById('txt-cor').innerHTML = 'N/A';
+            }
+        } else {
+            console.error("Erro ao buscar dados da espécie:", speciesApi.status);
+            document.getElementById('txt-cor').innerHTML = 'Erro';
+        }
+
+    } else {
+        
+        document.getElementById('txt-name').innerHTML = '';
+        document.getElementById('txt-id').innerHTML = '';
+        document.getElementById('img-perfil').src = 'perfil.png';
+        document.getElementById('txt-tipo').innerHTML = '';
+        document.getElementById('txt-cor').innerHTML = '';
     }
 }
-
-Buscarpokemon()
-
